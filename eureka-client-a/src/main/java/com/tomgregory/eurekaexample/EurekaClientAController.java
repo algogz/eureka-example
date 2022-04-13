669ae9d1166d489bad2142dc7ca46f958477a1f4
Tom Gregory <t.k.gregory@gmail.com>
2022-04-13 10:52:19 +0200

    Don't hard-code /whoami.

--- eureka-client-a/src/main/java/com/tomgregory/eurekaexample/EurekaClientAController.java
+++ eureka-client-a/src/main/java/com/tomgregory/eurekaexample/EurekaClientAController.java
@@ -1,19 +1,22 @@
 package com.tomgregory.eurekaexample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EurekaClientAController {
+    private String applicationName;
     private EurekaClientBClient serviceBClient;
 
-    public EurekaClientAController(EurekaClientBClient serviceBClient) {
+    public EurekaClientAController(@Value("${spring.application.name}") String applicationName, EurekaClientBClient serviceBClient) {
+        this.applicationName = applicationName;
         this.serviceBClient = serviceBClient;
     }
 
     @GetMapping(path = "whoami")
     public String whoami() {
-        return "You are calling serrvice-a!";
+        return String.format("You are calling %s!", applicationName);
     }
 
     @GetMapping(path = "call")
--- eureka-client-b/src/main/java/com/tomgregory/eurekaexample/EurekaClientBController.java
+++ eureka-client-b/src/main/java/com/tomgregory/eurekaexample/EurekaClientBController.java
@@ -1,19 +1,22 @@
 package com.tomgregory.eurekaexample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EurekaClientBController {
+    private String applicationName;
     private EurekaClientAClient serviceAClient;
 
-    public EurekaClientBController(EurekaClientAClient serviceAClient) {
+    public EurekaClientBController(@Value("${spring.application.name}") String applicationName, EurekaClientAClient serviceAClient) {
+        this.applicationName = applicationName;
         this.serviceAClient = serviceAClient;
     }
 
     @GetMapping(path = "whoami")
     public String whoami() {
-        return "You are calling serrvice-b!";
+        return String.format("You are calling %s!", applicationName);
     }
 
     @GetMapping(path = "call")
